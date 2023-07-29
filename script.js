c8f9a4d1e3f827b059283ee68b58011c5f9bdfb8
francium10 <134741973+francium10@users.noreply.github.com>
2023-07-29 12:29:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// Javascript for toggle meaning for closing and opening navbar in small king size
+let navLinks=document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.right="0";
+}
+function hideMenu(){
+    navLinks.style.right="-200px";
+}
